9dc4572a0e3fe298a6d55f05fda9f7ff4feeddd3
Adrian Knight <aknig2@uic.edu>
2022-06-22 15:51:26 -0500

    Title, clicker, and upgrade component files added

--- incremental/src/App.tsx
+++ incremental/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/Header'
+import ClickerButton from './components/ClickerButton';
+import UpgradeContainer from './components/UpgradeContainer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world, heres some text
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <ClickerButton />
+      <UpgradeContainer />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ incremental/src/components/UpgradeContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UpgradeContainer() {
+    return (
+        <div className='upgrade-container'>
+            Upgrades
+        </div>
+    )
+}
