af23034a5995a9e7064aca9a521f505228d3b292
Adrian Knight <aknig2@uic.edu>
2022-06-30 14:35:15 -0500

    UpgradeContainer added

--- incremental/src/components/UpgradeContainer.tsx
+++ incremental/src/components/UpgradeContainer.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function UpgradeContainer() {
     return (
         <div className='upgrade-container'>
-            Upgrades
+            <div className='upgrade-label'>Upgrades</div>
+            <div className='upgrades'>upgrade</div>
+            <div className='upgrades'>upgrade</div>
+            <div className='upgrades'>upgrade</div>
+            <div className='upgrades'>upgrade</div>
         </div>
     )
 }
