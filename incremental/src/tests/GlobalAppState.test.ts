1e17d0705705a3248baad824ce3aa50f9cdf6e1d
Ajknight121 <aknig2@uic.edu>
2022-07-18 11:51:12 -0500

    global state full coverage
    Added test for addToEmbersPerSec

--- incremental/src/tests/GlobalAppState.test.ts
+++ incremental/src/tests/GlobalAppState.test.ts
@@ -1,6 +1,20 @@
 import { GlobalAppState } from "../model/GlobalAppState";
 /** The global app state is our SUT aka subject under test, so all assertions in this test suite should be on our SUT */
 describe("global app state", () => {
+  it("addToEmbersPerSec", ()=> {
+    //Arrange
+    const epsToAdd = 1;
+    const mockEmbersPerSecond = 0;
+    const mockEmbers = 10;
+    let mockState = new GlobalAppState();
+    mockState.embers = mockEmbers;
+    mockState.embersPerSecond = mockEmbersPerSecond;
+    //Act
+    const newState = GlobalAppState.addToEmbersPerSec(epsToAdd, mockState);
+    //Assert
+    expect(newState.embersPerSecond).toBe(epsToAdd);
+    expect(newState.embers).toBe(mockEmbers);
+  })
   it("addEmbersPerSecondOnTick", () => {
     //Arrange
     const mockEmbersPerSecond = 5;
